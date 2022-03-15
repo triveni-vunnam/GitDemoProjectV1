4d070133c0244af2980540f43478cf7ce0f654e1
tvunnam <tvunnam@IN-PG02M1C6>
2022-03-15 12:02:48 +0530

    FIRST COMMIT

---
+++ src/HelloWorld.java
@@ -0,0 +1,25 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		int myInteger =4567;
+		short myShort =54;
+		byte myByte = 127;
+		float myFloat=123.8f;
+		double myDouble=6578.9;
+		char character='t';
+		boolean myBoolean=false;
+		long number=678902134;
+		
+		System.out.println(myInteger);
+		System.out.println(myShort);
+		System.out.println(myByte);
+		System.out.println(myFloat);
+		System.out.println(myDouble);
+		System.out.println(character);
+		System.out.println(myBoolean);
+		System.out.println(number);
+	}
+
+}
