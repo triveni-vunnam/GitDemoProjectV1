ba580f709efd20dd5c9ac919b61414cad4206d0b
triveni-vunnam <101560258+triveni-vunnam@users.noreply.github.com>
2023-03-18 10:19:02 +0530

    Added code in HiClass

--- src/HiClass.java
+++ src/HiClass.java
@@ -3,6 +3,10 @@ public class HiClass {
 
 	public static void main(String[] args) {
 		System.out.println("hi");
+		System.out.println("hi hello");
+		System.out.println("hi hello hi");
+		
+		
 
 	}
 
